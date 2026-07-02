c68905d9588587a8770acfa728cc7adf9e9fb337
sudeepa77 <sudeepareddy295@gmail.com>
2026-07-02 14:23:44 +0530

    add exercise 3

---
+++ Exercise-3-SpringAOP/src/main/java/com/library/aspect/LoggingAspect.java
@@ -0,0 +1,24 @@
+package com.library.aspect;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+
+@Aspect
+public class LoggingAspect {
+
+    @Around("execution(* com.library.service.*.*(..))")
+    public Object logExecutionTime(ProceedingJoinPoint joinPoint) throws Throwable {
+
+        long start = System.currentTimeMillis();
+
+        Object result = joinPoint.proceed();
+
+        long end = System.currentTimeMillis();
+
+        System.out.println("Method: " + joinPoint.getSignature().getName()
+                + " executed in " + (end - start) + " ms");
+
+        return result;
+    }
+}
